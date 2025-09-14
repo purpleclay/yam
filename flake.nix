e1fc63f46ecf5c12540d8e667ed790aee791b043
purpleclay <purpleclaygh@gmail.com>
2025-09-14 08:08:00 +0100

    chore: initial project setup

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "Context-aware YAML to markdown document generator";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        overlays = [(import rust-overlay)];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+
+        rustToolchain = pkgs.rust-bin.stable."1.89.0".default.override {
+          extensions = ["rust-src" "cargo" "rustc" "clippy" "rustfmt"];
+        };
+
+        buildInputs = with pkgs; [
+          alejandra
+          nil
+          typos
+        ];
+
+        nativeBuildInputs = with pkgs; [
+          rustToolchain
+          pkg-config
+        ];
+      in
+        with pkgs; {
+          devShells.default = mkShell {
+            inherit buildInputs nativeBuildInputs;
+          };
+        }
+    );
+}
