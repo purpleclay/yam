cbb0944361943b59e08392dc3ea2d337bd1f43a5
purpleclay <purpleclaygh@gmail.com>
2025-09-21 06:41:07 +0100

    feat: switch to using a positional argument and include parsing yaml from stdin

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,35 @@
 mod parser;
 
 use anyhow::{Context, Result};
-use clap::{Parser, Subcommand};
-use std::fs;
+use clap::Parser;
+use std::{
+    fs,
+    io::{self, Read},
+};
 
 use crate::parser::parse;
 
 #[derive(Parser, Debug)]
 #[command(author)]
 struct Args {
-    #[command(subcommand)]
-    commands: Option<Commands>,
-}
-
-#[derive(Subcommand, Debug)]
-enum Commands {
-    /// Generate markdown from a given YAML file
-    Generate {
-        /// Path to the YAML file
-        #[arg(short, long)]
-        file: String,
-    },
+    #[arg(value_name = "FILE")]
+    file: String,
 }
 
 fn main() -> Result<()> {
     let args = Args::parse();
 
-    match args.commands {
-        Some(Commands::Generate { file }) => {
-            let content = fs::read_to_string(&file)
-                .with_context(|| format!("failed to read file: {}", file))?;
-            let document = parse(&content)?;
-            println!("{}", document);
-            Ok(())
-        }
-        None => Ok(()),
+    let mut content = String::new();
+    if args.file == "-" {
+        io::stdin()
+            .read_to_string(&mut content)
+            .context("Failed to read from stdin")?;
+    } else {
+        content = fs::read_to_string(&args.file)
+            .with_context(|| format!("failed to read file: {}", args.file))?;
     }
+
+    let document = parse(&content)?;
+    println!("{}", document);
+    Ok(())
 }
