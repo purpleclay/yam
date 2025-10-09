f3fe8dad89e75ec4665e7f994c37f61c8ebc679b
purpleclay <purpleclaygh@gmail.com>
2025-10-09 06:35:02 +0100

    ci: ensure cargo patch script is compatible with an ubuntu runner

--- scripts/patch-cargo-version.sh
+++ scripts/patch-cargo-version.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Patch the Cargo.toml version using nsv
 
-sed -i '' "s/^version = \"$NSV_PREV_TAG\"/version = \"$NSV_NEXT_TAG\"/" \
+sed -i "s/^version = \"$NSV_PREV_TAG\"/version = \"$NSV_NEXT_TAG\"/" \
   "$NSV_WORKING_DIRECTORY/Cargo.toml"
